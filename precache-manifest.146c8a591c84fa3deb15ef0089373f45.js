7d32e1bcc3250aa038969a83fcf0a78866fdeed6
subramanya <kssubramanyanayak1997@gmail.com>
2020-11-06 14:11:50 +0530

    Updates

---
+++ precache-manifest.146c8a591c84fa3deb15ef0089373f45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43360e7f5ba209edac94579053c25f0",
+    "url": "/sec-app/index.html"
+  },
+  {
+    "revision": "0d90382f0259969bbcc7",
+    "url": "/sec-app/static/css/main.54446425.chunk.css"
+  },
+  {
+    "revision": "68dc0a546a7d8f6e6c1e",
+    "url": "/sec-app/static/js/2.9afc71e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sec-app/static/js/2.9afc71e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d90382f0259969bbcc7",
+    "url": "/sec-app/static/js/main.277d984e.chunk.js"
+  },
+  {
+    "revision": "5f43dedb5f7035d3ab0b",
+    "url": "/sec-app/static/js/runtime-main.2a0ebf84.js"
+  },
+  {
+    "revision": "b14bf5f0daa20ef6b39551c7c566b109",
+    "url": "/sec-app/static/media/1-10097_powerpoint-checkmark-symbol-hd-png-download.b14bf5f0.png"
+  },
+  {
+    "revision": "fb8514c12e900b4ce83ed8e1d0d576fe",
+    "url": "/sec-app/static/media/wolfgang-hasselmann-pVr6wvUneMk-unsplash.fb8514c1.jpg"
+  }
+]);
